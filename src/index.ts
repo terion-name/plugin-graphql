3fa0840d38ceaecb4fa510f080f1c4c7afe1b1ad
terion <mail@terion.name>
2021-06-16 14:20:56 +0300

    export QueryBuilder

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import VuexORMGraphQLPlugin from "./plugin";
 import DefaultAdapter from "./adapters/builtin/default-adapter";
 import Adapter, { ConnectionMode, ArgumentMode } from "./adapters/adapter";
 import Model from "./orm/model";
+import QueryBuilder from "./graphql/query-builder";
 export default VuexORMGraphQLPlugin;
 
 export { setupTestUtils, mock, clearORMStore, Mock, MockOptions, ReturnValue } from "./test-utils";
-export { DefaultAdapter, Adapter, ConnectionMode, ArgumentMode, Model };
+export { DefaultAdapter, Adapter, ConnectionMode, ArgumentMode, Model, QueryBuilder };
